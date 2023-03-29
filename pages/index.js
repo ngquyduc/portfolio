f498b59b76a7742a7423d5ed219de748e16b0b27
Nguyen Quy Duc <quyduc711@gmail.com>
2023-03-29 23:55:22 +0800

    Add Home and About pages

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
-
+import Main from '@/components/Main'
+import About from '@/components/About'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -15,6 +16,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar/>
+      <Main/>
+      <About/>
     </>
   )
 }
